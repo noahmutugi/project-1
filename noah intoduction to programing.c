9c72f16fd9cf25b3f61f7e848ab044795bee8bbf
Noah Mutugi <noahmutugike@gmail.com>
2024-10-15 19:16:53 +0300

    chapter 2 code

--- noah intoduction to programing.c
+++ noah intoduction to programing.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(void)
 {
-    printf("this is output from my first program!\n");return 0;
+printf("This is output from my first program!\n");
+return 0;
 }
